984719f27d49b27f587ecbbbf30d848d8c3d5202
fuentes89468 <fuentesaldanamagali@gmail.com>
2023-10-11 11:00:03 -0300

    changes

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,9 +1,3 @@
 export default {
   activeBar: (state) => state.activeBar,
-  isLoading: (state) => (key) => {
-    return state.loading[key] !== undefined ? state.loading[key] : false;
-  },
-  presentationData: (state) => state.presentationData,
-  experienceData: (state) => state.experienceData,
-  educationData: (state) => state.educationData,
 };
